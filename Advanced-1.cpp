8b26bfeeb58f09e6cc294f4fd1077e140900c343
Baraa <156128855+Baraa-op@users.noreply.github.com>
2025-08-29 11:38:51 +0300

    Create Advanced-1.cpp

---
+++ Advanced-1.cpp
@@ -0,0 +1,13 @@
+//advanced solution - 1
+#include<iostream>
+using namespace std;
+
+void PrintName(string Name)
+{
+	cout << "your name is : " << Name << endl;
+}
+
+int main()
+{
+	PrintName("baraa") ;
+}
