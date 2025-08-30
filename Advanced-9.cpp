a093f38e538a1408ee9495a12183a0d18777324f
Baraa <156128855+Baraa-op@users.noreply.github.com>
2025-08-30 21:43:23 +0300

    Create Advanced-9.cpp

---
+++ Advanced-9.cpp
@@ -0,0 +1,39 @@
+// advanced solution-8
+#include<iostream>
+using namespace std;
+
+
+void Read_Num(int &Num1,int &Num2,int&Num3)
+{
+	cout << "enter num 1 : " ;
+	cin >> Num1 ;
+	
+	cout << "enter num 2 : " ; 
+	cin >> Num2 ;
+	
+	cout << "enter num 3 : " ;
+	cin >> Num3 ;
+}
+
+
+int SumOf3Num(int &Num1,int &Num2,int&Num3)
+{
+	return Num1 + Num2 + Num3 ;
+	
+}
+
+void PrintResult(int total)
+{
+	cout << "\ntotal of sum : " << total << endl ;
+}
+
+int main()
+{
+	int Num1 , Num2 , Num3 ;
+	Read_Num(Num1 , Num2 ,Num3) ;
+	PrintResult(SumOf3Num(Num1 , Num2 ,Num3)) ;
+	
+	
+
+	return 0 ;
+}
